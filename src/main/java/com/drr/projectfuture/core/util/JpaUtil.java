2e30d19caccc76b942141c19dcb83510923dff15
david154x <david154x@hotmail.com>
2023-02-18 12:17:22 -0500

    Version inicial de nuestro proyecto

--- src/main/java/com/drr/projectfuture/core/util/JpaUtil.java
+++ src/main/java/com/drr/projectfuture/core/util/JpaUtil.java
@@ -1,8 +1,19 @@
 package com.drr.projectfuture.core.util;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class JpaUtil {
 
-	
-	
+	private static final EntityManagerFactory entityManagerFactory = buildEntityManagerFactory();
+
+    private static EntityManagerFactory buildEntityManagerFactory(){
+        return Persistence.createEntityManagerFactory("hibernate");
+    }
+
+    public static EntityManager getEntityManager() {
+        return entityManagerFactory.createEntityManager();
+    }
 	
 }
